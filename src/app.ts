ec4a0358765023c03a604f2ce077564456f29308
Andrés <lopez.andresrigoberto@gmail.com>
2023-04-18 16:51:14 -0500

    [add] autobind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,17 @@
+function autobind(_:any, _2: string, descriptior: PropertyDescriptor) {
+    const originalMethod = descriptior.value;
+
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+ 
+}
+
 class ProjectInput {
 
     templateElement: HTMLTemplateElement;
@@ -23,6 +37,7 @@ class ProjectInput {
         this.attach();
     }
 
+    @autobind
     private submitHandler(event: Event) {
         event.preventDefault();
     }
